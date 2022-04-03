a316579c856a96311e61660425ffe510482a03c0
Anton <anton_sonic@inbox.ru>
2022-04-03 20:14:56 +0300

    fixed swippers, added folders

---
+++ js/script-carousel.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 50,
+        dots: false,
+        nav: false,
+        items: 4,
+        stagePadding: 20,
+    });
+});
