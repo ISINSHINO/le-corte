74e2d0ed54ed33421cf52fcbb517083db5880938
Anton <anton_sonic@inbox.ru>
2022-04-04 23:13:19 +0300

    item counter, clickable buttons, local fixes

--- js/script.js
+++ js/script.js
@@ -1,39 +1,3 @@
-/* Индекс слайда по умолчанию */
-var slideIndex = 1;
-showSlides(slideIndex);
-
-/* Функция увеличивает индекс на 1, показывает следующй слайд*/
-function plusSlide() {
-    showSlides(slideIndex += 1);
-}
-
-/* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
-function minusSlide() {
-    showSlides(slideIndex -= 1);
-}
-
-/* Устанавливает текущий слайд */
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-/* Основная функция сладера */
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("item");
-    if (n > slides.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-
-    slides[slideIndex - 1].style.display = "block";
-}
-
 function myFunction() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");;
@@ -48,4 +12,13 @@ function confirmingAge() {
     popupAge.classList.toggle("active");
     var blur = document.querySelector(".age-container");
     blur.classList.toggle("blur");
+}
+
+
+function incrementValue() {
+    var value = parseInt(document.getElementById('basket-items').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('basket-items').value = value;
+    document.getElementById('basket-items').style.display = 'inline-block';
 }
